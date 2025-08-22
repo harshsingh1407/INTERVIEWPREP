dc0e32904b86bb0b2c1de157468296d2ee488524
Harsh <harshsingh95941@gmail.com>
2025-08-22 23:07:37 +0530

    Update Origin

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,9 @@ const app = express();
 // Middleware to handle CORS
 
 app.use(cors({
-    origin:"*",
+    // origin:"*",
+    origin: ["https://interviewedge.netlify.app/"], // frontend domain
+    credentials: true,
     methods:["GET","POST","PUT","DELETE"],
     allowedHeaders:["Content-Type","Authorization"],
 }));
