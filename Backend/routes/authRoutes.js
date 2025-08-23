e224e310f573ddf8dc924805c557ccd1954c31cf
Harsh <harshsingh95941@gmail.com>
2025-08-23 20:51:31 +0530

    Fixing User Icon Bug (attempt 1)

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,21 +1,30 @@
 const express = require("express")
-const {registerUser, loginUser, getUserProfile} = require("../controller/authController")
-const {protect} = require("../middlewares/authMiddleware")
+const { registerUser, loginUser, getUserProfile } = require("../controller/authController")
+const { protect } = require("../middlewares/authMiddleware")
 const upload = require('../middlewares/uploadsMiddleware')
 
 const router = express.Router();
 
 // Auth Routes
-router.post('/register',registerUser);
-router.post('/login',loginUser);
-router.get('/profile',protect,getUserProfile);
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/profile', protect, getUserProfile);
 
-router.post('/upload-image',upload.single('image'),(req,res)=>{
-    if(!req.file) {
-        return res.status(400).json({message:"No file uploaded!"})
+router.post('/upload-image', upload.single('image'), (req, res) => {
+
+    const host = req.get("host");
+    let protocol = req.protocol;
+
+    // Force https in production (Render uses HTTPS)
+    if (host.includes("onrender.com")) {
+        protocol = "https";
     }
-    const imageURL = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`
-    res.status(200).json({imageURL})
+
+    if (!req.file) {
+        return res.status(400).json({ message: "No file uploaded!" })
+    }
+    const imageURL = `${protocol}://${host}/uploads/${req.file.filename}`
+    res.status(200).json({ imageURL })
 })
 
 module.exports = router; 
