e6ec5b180a7c4e049c5f3fdb3b71d5404f352ac3
Harsh <harshsingh95941@gmail.com>
2025-08-23 21:10:47 +0530

    Update Login Form

--- Frontend/src/pages/Auth/Login.jsx
+++ Frontend/src/pages/Auth/Login.jsx
@@ -12,7 +12,7 @@ const Login = ({setcurrentPage}) => {
   const [email, setemail] = useState("");
   const [password, setpassword] = useState("");
   const [error, seterror] = useState(null);
-  const [isLoading, setisLoading] = useState(false);
+  const [isLoading, setisLoading] = useState(false)
 
   const {updateUser} = useContext(UserContext)
 
@@ -31,7 +31,7 @@ const Login = ({setcurrentPage}) => {
     }
     
     seterror("");
-    setisLoading(true);
+    setisLoading(true)
 
     try {
       const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
@@ -50,6 +50,8 @@ const Login = ({setcurrentPage}) => {
       } else {
         seterror("Something went wrong. Please try again!")
       }
+    } finally {
+      setisLoading(false)
     }
   }
 
@@ -63,7 +65,14 @@ const Login = ({setcurrentPage}) => {
         {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
         <button type='submit' className='btn-primary'>Login</button>
         <p className='text-[13px] text-slate-800 mt-3'>Don't have an account?{""}
-          <button className='font-medium text-primary underline cursor-pointer' onClick={()=> { setcurrentPage("signup")}} disabled={isLoading}>{isLoading && <SpinnerLoader/>} Signup</button>
+          <button className='font-medium text-primary underline cursor-pointer' onClick={()=> { setcurrentPage("signup")}} disabled={isLoading}>{isLoading ? (
+            <>
+              <SpinnerLoader />
+              <span>Logging in...</span>
+            </>
+          ) : (
+            "Login"
+          )}</button>
         </p>
       </form>
     </div>
