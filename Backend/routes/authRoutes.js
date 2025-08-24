94213cf033c6ca4ee16e9ccf29a6b9b65e876bc4
Harsh <harshsingh95941@gmail.com>
2025-08-24 22:16:43 +0530

    Fix Bug

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -10,18 +10,18 @@ router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/profile', protect, getUserProfile);
 
-router.post("/upload-profile", upload.single("image"), async (req, res) => {
-  try {
-    const imageUrl = req.file.path; // ⚡ Cloudinary gives this directly
-    // Save in MongoDB
-    user.profileImageUrl = imageUrl;
-    await user.save();
+// router.post("/upload-profile", upload.single("image"), async (req, res) => {
+//   try {
+//     const imageUrl = req.file.path; // ⚡ Cloudinary gives this directly
+//     // Save in MongoDB
+//     user.profileImageUrl = imageUrl;
+//     await user.save();
 
-    res.json({ imageURL: imageUrl });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+//     res.json({ imageURL: imageUrl });
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
 
 // router.post("/upload-image", upload.single("profileImage"), (req, res) => {
 //   try {
@@ -32,21 +32,21 @@ router.post("/upload-profile", upload.single("image"), async (req, res) => {
 //   }
 // });
 
-// router.post('/upload-image', upload.single('image'), (req, res) => {
+router.post('/upload-image', upload.single('image'), (req, res) => {
 
-//     const host = req.get("host");
-//     let protocol = req.protocol;
+    const host = req.get("host");
+    let protocol = req.protocol;
 
-//     // Force https in production (Render uses HTTPS)
-//     if (host.includes("onrender.com")) {
-//         protocol = "https";
-//     }
+    // Force https in production (Render uses HTTPS)
+    if (host.includes("onrender.com")) {
+        protocol = "https";
+    }
 
-//     if (!req.file) {
-//         return res.status(400).json({ message: "No file uploaded!" })
-//     }
-//     const imageURL = `${protocol}://${host}/uploads/${req.file.filename}`
-//     res.status(200).json({ imageURL })
-// })
+    if (!req.file) {
+        return res.status(400).json({ message: "No file uploaded!" })
+    }
+    const imageURL = `${protocol}://${host}/uploads/${req.file.filename}`
+    res.status(200).json({ imageURL })
+})
 
 module.exports = router; 
